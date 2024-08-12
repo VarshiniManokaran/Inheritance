f14e3fed28a25deb3916cf0876553750e2996586
VarshiniManokaran <varshini6678@gmail.com>
2024-08-12 16:12:07 +0530

    Add files via upload

---
+++ Restaurant.java
@@ -0,0 +1,43 @@
+class chineseFood{
+    int momos=4;
+
+    public static void like()
+    {
+    
+        System.out.println("I like Momos Very Much");
+    }
+    
+    public static void Noodles(){
+        System.out.println("I like Noodles and my fav one ");
+    }
+
+
+}
+class  indianFood extends chineseFood{
+    //boolean  ieatChicken=true;
+    int mutton=6;
+    
+}
+ class Restaurant{
+    public static void main(String[] args) {
+        chineseFood c1=new chineseFood();
+        System.out.println(c1.momos);
+       
+    
+        c1.like();
+        c1.Noodles();
+        
+
+    }
+
+}
+
+
+    
+
+
+
+
+
+
+
